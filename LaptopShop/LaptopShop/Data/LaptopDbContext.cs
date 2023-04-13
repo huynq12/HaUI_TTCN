102dac3fd05844d9dcc871c7aa68bde58b0d6168
qYuhhhhh <ngquanghuy52@gmail.com>
2023-04-13 21:33:36 +0700

    add cart model

--- LaptopShop/LaptopShop/Data/LaptopDbContext.cs
+++ LaptopShop/LaptopShop/Data/LaptopDbContext.cs
@@ -14,6 +14,8 @@ namespace LaptopShop.Data
 		public DbSet<Category> LaptopCategories { get; set; }
 		public DbSet<Order> Orders { get; set; }
 		public DbSet<OrderDetail> OrderDetails { get; set; }
+		public DbSet<Cart> Carts { get; set; }
+		public DbSet<CartItem> CartItems { get; set; }
 
 	}
 }
--- LaptopShop/LaptopShop/Models/EF/CartItem.cs
+++ LaptopShop/LaptopShop/Models/EF/CartItem.cs
@@ -2,5 +2,9 @@
 {
 	public class CartItem
 	{
+		public int ItemId { get; set; }
+		public Laptop Laptop { get; set; }
+		public int Amount { get; set; }
+		public string CartId { get; set; }
 	}
 }
