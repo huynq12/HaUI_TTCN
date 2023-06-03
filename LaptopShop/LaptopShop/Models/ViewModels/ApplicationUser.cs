055d51312579942cfc6539bd1acec2af07420029
qYuhhhhh <ngquanghuy52@gmail.com>
2023-06-03 23:13:08 +0700

    authentication

--- LaptopShop/LaptopShop/Models/ViewModels/ApplicationUser.cs
+++ LaptopShop/LaptopShop/Models/ViewModels/ApplicationUser.cs
@@ -4,8 +4,8 @@ namespace LaptopShop.Models.ViewModels
 {
 	public class ApplicationUser : IdentityUser
 	{
-		public string FirstName { get; set; }
-		public string LastName { get; set; }
+		public string? FirstName { get; set; }
+		public string? LastName { get; set; }
 		public string? ProfilePicture { get; set; }
 	}
 }
